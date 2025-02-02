fbc3bf7f3edb104a5245a97b5c1bc68647641f51
Shahab Dogar <shahab@dogar.dev>
2025-02-02 18:08:14 +0500

    fix: NixOS | switch completely to pipewire

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -98,17 +98,13 @@ in
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
+  sound.enable = false;
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   services.udev.extraRules = ''
@@ -167,7 +163,12 @@ in
   };
 
   # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs = {
+    config = {
+      pulseaudio = false;
+      allowUnfree = true;
+    };
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
