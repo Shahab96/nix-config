f540ad38bdeb47b2c32178ebbf4040f81a5992c9
Shahab Dogar <shahab@dogar.dev>
2025-02-04 08:49:38 +0500

    fix: NixOS | incorrect command for tuigreet

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -63,7 +63,7 @@ in
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --greeting 'Welcome to NixOS!' --asterisks --remember --remember-user-session --time -cmd ${pkgs.hyprland}/bin/Hyprland";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --greeting 'Welcome to NixOS!' --asterisks --remember --remember-user-session --time --cmd ${pkgs.hyprland}/bin/Hyprland";
         user = "greeter";
       };
     };
