bba65699c6b2aa5cee516a477bdd8d920178e987
Shahab Dogar <shahab@dogar.dev>
2025-02-14 21:17:56 +0500

    feat: NixOS | enable yubikey auth and login

--- nixos/imports/security.nix
+++ nixos/imports/security.nix
@@ -1,5 +1,17 @@
 {...}: {
   security = {
     rtkit.enable = true;
+    pam = {
+      u2f = {
+        enable = true;
+        settings = {
+          cue = true;
+        };
+      };
+      services = {
+        sudo.u2fAuth = true;
+        login.u2fAuth = true;
+      };
+    };
   };
 }
