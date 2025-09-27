23ddc84bfc3a2d7b2ea92dab970a51282f52b692
Shahab Dogar <shahab@dogar.dev>
2025-09-27 22:27:50 +0500

    feat: make all hyprland config files symlinks

--- home/shahab/common/optional/hyprland.nix
+++ home/shahab/common/optional/hyprland.nix
@@ -1,16 +1,19 @@
-{ config, lib, pkgs, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
   home = {
     file = {
       "${config.xdg.configHome}/hypr".source =
-        lib.custom.relativeToRoot "dotfiles/hypr";
+        config.lib.file.mkOutOfStoreSymlink "${lib.custom.relativeToRoot "dotfiles/hypr"}";
       "${config.xdg.configHome}/waybar".source =
-        lib.custom.relativeToRoot "dotfiles/waybar";
+        config.lib.file.mkOutOfStoreSymlink "${lib.custom.relativeToRoot "dotfiles/waybar"}";
       "${config.xdg.configHome}/wofi".source =
-        lib.custom.relativeToRoot "dotfiles/wofi";
+        config.lib.file.mkOutOfStoreSymlink "${lib.custom.relativeToRoot "dotfiles/wofi"}";
       "${config.xdg.configHome}/mako".source =
-        lib.custom.relativeToRoot "dotfiles/mako";
+        config.lib.file.mkOutOfStoreSymlink "${lib.custom.relativeToRoot "dotfiles/mako"}";
     };
 
     packages = with pkgs; [
